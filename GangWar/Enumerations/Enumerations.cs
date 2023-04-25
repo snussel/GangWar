d6d0a8d1dfb61cdf61dc7f4f3cf77a2624875b5b
Steven Nussel <Durendal@hotmail.com>
2023-04-25 12:27:18 -0400

    Skill Update

--- GangWar/Enumerations/Enumerations.cs
+++ GangWar/Enumerations/Enumerations.cs
@@ -6,12 +6,32 @@ using System.Threading.Tasks;
 
 namespace GangWar.Enumerations
 {
-    public enum EquipmentType { Weapon }
+    public enum EquipmentType 
+    { 
+        Armor,
+        Weapon,
+        Consumable,
+        Other
+    }
 
-    public enum QualityLevels { }
+    public enum QualityLevels 
+    {
+        Bad = 1,
+        Poor = 2,
+        Average = 3,
+        Good = 4,
+        Excellent = 5
+    }
 
     public enum SkillTypes 
     { 
-        Movement = 1, Speed = 2, Melee = 3, Ranged = 4, Strength = 5, Toughness = 6, Alertness = 7, Charisma = 8
+        Movement = 1,
+        Speed = 4,
+        Melee = 2, 
+        Ranged = 3,
+        Strength = 5,
+        Toughness = 6, 
+        Alertness = 7, 
+        Charisma = 8
     }
 }
